c4dbf879df266e934656ea94eeb18904d861f74a
huxiaodi <huxiaodi@qiatuchina.com>
2018-02-02 16:50:20 +0800

    实现最基本访问

--- src/main/java/com/springboot/manager/ManagerApplication.java
+++ src/main/java/com/springboot/manager/ManagerApplication.java
@@ -1,12 +1,29 @@
 package com.springboot.manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ManagerApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 	}
+
+
+	/*public static void main(String[] args) throws Exception {
+		SpringApplication.run(TestBootController.class, args);
+	}*/
 }
