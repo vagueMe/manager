6c355c8b6569bfa4e9bb200696fe16789a7e507e
胡迪 <huxiaodi@qiatuchina.com>
2018-02-06 15:53:16 +0800

    引入 lombok 方便get和set <注：idea 需要安装lombok插件>

---
+++ src/main/java/com/springboot/manager/model/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.springboot.manager.model.dto;
+
+import lombok.Data;
+
+/**
+ * @Description：
+ * @Author:hudi
+ * @Date: Created in 15:40 2018/2/6
+ */
+@Data
+public class UserDto {
+
+    private  String userId;
+
+    private String userName;
+
+    private int age;
+
+
+}
---
+++ src/test/java/com/springboot/manager/common/LombokGetSetTest.java
@@ -0,0 +1,28 @@
+package com.springboot.manager.common;
+
+import com.springboot.manager.model.dto.UserDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Description：
+ * @Author:hudi
+ * @Date: Created in 15:42 2018/2/6
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LombokGetSetTest {
+
+    @Test
+    public void test1(){
+        UserDto userDto = new UserDto();
+        userDto.setAge(1);
+        userDto.setUserId("id");
+        userDto.setUserName("name");
+
+        System.out.println(userDto.toString());
+    }
+
+}
