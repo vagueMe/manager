3392912e6cf8f4493b57080be166ff0081989040
胡迪 <huxiaodi@qiatuchina.com>
2018-02-06 16:01:29 +0800

    日志测试类

--- src/test/java/com/springboot/manager/common/LoggerTest.java
+++ src/test/java/com/springboot/manager/common/LoggerTest.java
@@ -1,5 +1,6 @@
-package com.springboot.manager;
+package com.springboot.manager.common;
 
+import lombok.extern.log4j.Log4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -15,15 +16,19 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Test1 {
+@Log4j
+public class LoggerTest {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public  void test1  (){
+
         logger.debug("111");
         logger.info("222");
         logger.error("333");
+
+        log.info("111"); // 这里是使用的是 lombak 的@Log4j 注解生成的log对象  注：在使用lombak 之前idea要先安装插件
     }
 
 }
