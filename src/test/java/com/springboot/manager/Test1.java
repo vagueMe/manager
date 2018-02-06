d7bd171ea4edc63cc03857c444168f2ae6c41ea5
huxiaodi <huxiaodi@qiatuchina.com>
2018-02-06 14:21:56 +0800

    原生日志测试

--- src/test/java/com/springboot/manager/Test1.java
+++ src/test/java/com/springboot/manager/Test1.java
@@ -1,9 +1,29 @@
 package com.springboot.manager;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
 /**
  * @Description：
  * @Author:hudi
  * @Date: Created in 9:03 2018/2/6
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class Test1 {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public  void test1  (){
+        logger.debug("111");
+        logger.info("222");
+        logger.error("333");
+    }
+
 }
