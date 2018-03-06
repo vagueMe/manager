7d123b4af9558c799aa05b4fbe5c08b5cd61b091
胡迪 <huxiaodi@qiatuchina.com>
2018-03-06 09:53:50 +0800

    初始化vue

---
+++ mvue/src/components/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h5>11</h5>
+    <h5>{{value1}},{{value2}}</h5>
+    <h5>{{value1 | matchFilter }}</h5>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "father",
+    data () {
+      return {
+      }
+    },
+    props: ['value1','value2'],
+    filters: {
+      matchFilter:function (param) {
+        return "this is a filter";
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ mvue/src/components/Test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <span :class="{'a':'A','b':'B','c':'C','d':'D'}[msg]">this is test</span>
+    <father value1="hahah" :value2 = "value2"></father>
+  </div>
+</template>
+
+<script>
+import fatherVue from './ChildComponent.vue'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'a',
+      value2:"ddddd"
+    }
+  },
+  components: {
+    'father': fatherVue
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.A{
+  color: red;
+}
+.B{
+  color:black;
+}
+.C{
+  color: green;
+}
+.D{
+  color: yellow;
+}
+</style>
